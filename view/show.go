457f1d0781795476ffb9e5f540e0acbb0d1bc976
navleorange <05080161@gmail.com>
2022-01-22 18:19:41 +0900

    整形プリントの実装

---
+++ view/show.go
@@ -0,0 +1,81 @@
+package view
+
+import (
+	"fmt"
+
+	"sort"
+	"time"
+
+	"github.com/arumakan1727/taskrader/assignment"
+	color "github.com/fatih/color"
+)
+
+/*
+Origin   Origin    // 課題の出所
+	Title    string    // 課題のタイトル
+	Course   string    // 科目orコース名
+	Deadline time.Time // 課題の締め切り; 不明の場合は UnknownDeadline() を設定する
+*/
+
+/*
+n件の未提出課題：
+[index]	Title
+Deadline	@ + Origin
+*/
+
+func Show(tasks []*assignment.Assignment) {
+	const formatHour = "2006/01/02"
+	const formatMin = "15:04"
+	wdays := []string{"日", "月", "火", "水", "木", "金", "土"}
+
+	sort.Slice(tasks, func(i, j int) bool {
+		return tasks[i].Deadline.Before(tasks[j].Deadline)
+	})
+
+	line := makeLine(tasks)
+
+	fmt.Println(len(tasks), "件の未提出課題：")
+	for i := 0; i < len(tasks); i++ {
+
+		limit := "<締切不明>"
+
+		if tasks[i].Deadline != time.Unix(1<<60, 999999999) {
+			limit = tasks[i].Deadline.Format(formatHour) + " (" + wdays[tasks[i].Deadline.Weekday()] + ") " + tasks[i].Deadline.Format(formatMin)
+		}
+
+		fmt.Println(line)
+		output := fmt.Sprintf("| [%d] %s |\n|     %s @%s |", i+1, tasks[i].Title, limit, tasks[i].Origin)
+		printWithColor(output, tasks[i].Deadline)
+	}
+
+	fmt.Println(line)
+
+}
+
+func makeLine(tasks []*assignment.Assignment) string {
+	max := 0
+
+	for i := 0; i < len(tasks); i++ {
+		if max < len(tasks[i].Title) {
+			max = len(tasks[i].Title)
+		}
+	}
+
+	line := ""
+
+	for i := 0; i < max+4; i++ {
+		line += "-"
+	}
+
+	return line
+}
+
+func printWithColor(output string, deadline time.Time) {
+	if diff := time.Until(deadline).Hours(); 0 <= diff && diff <= 24 {
+		fmt.Println(color.RedString(output))
+	} else if 24 < diff && diff <= 48 {
+		fmt.Println(color.YellowString(output))
+	} else {
+		fmt.Println(output)
+	}
+}
