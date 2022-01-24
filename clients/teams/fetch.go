60418eaaffa5570a83a13a024eb6aac7d7d82a1a
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-01-24 13:26:02 +0900

    fix: Teamsの締切に "明日" や "tomorrow" が使われていても正しくパースできるように

--- clients/teams/fetch.go
+++ clients/teams/fetch.go
@@ -129,8 +129,31 @@ func scrapeAssignmentList(html string, logger *log.Logger) ([]Assignment, error)
 
 func parseDueText(text string) (time.Time, error) {
 	if strings.HasPrefix(text, "Due") {
+		if strings.HasPrefix(text, "Due tomorrow at") {
+			replaceTo := time.Now().AddDate(0, 0, 1).Format("January 2, 2006")
+			text = strings.Replace(text, "tomorrow at", replaceTo, 1)
+
+		} else if strings.HasPrefix(text, "Due today at") {
+			replaceTo := time.Now().Format("January 2, 2006")
+			text = strings.Replace(text, "today at", replaceTo, 1)
+		}
 		return time.ParseInLocation("Due January 2, 2006 3:04 PM", text, time.Local)
-	} else if strings.HasPrefix(text, "期限") {
+	}
+	if strings.HasPrefix(text, "明日") {
+		text = strings.TrimPrefix(text, "明日")
+		text = strings.TrimSuffix(text, "が期限")
+		text = strings.TrimSpace(text)
+		t := time.Now().AddDate(0, 0, 1)
+		text = "期限" + t.Format(" 2006年1月2日 ") + text
+	}
+	if strings.HasPrefix(text, "今日") {
+		text = strings.TrimPrefix(text, "今日")
+		text = strings.TrimSuffix(text, "が期限")
+		text = strings.TrimSpace(text)
+		t := time.Now()
+		text = "期限" + t.Format(" 2006年1月2日 ") + text
+	}
+	if strings.HasPrefix(text, "期限") {
 		return time.ParseInLocation("期限 2006年1月2日 15:04", text, time.Local)
 	}
 	return time.Time{}, fmt.Errorf("unknown dueText format: %s", text)
---
+++ clients/teams/scrape_test.go
@@ -0,0 +1,57 @@
+package teams
+
+import (
+	"testing"
+	"time"
+)
+
+func tomorrowAt(hour, min int) time.Time {
+	t := time.Now().AddDate(0, 0, 1)
+	return time.Date(t.Year(), t.Month(), t.Day(), hour, min, 0, 0, time.Local)
+}
+
+func todayAt(hour, min int) time.Time {
+	t := time.Now()
+	return time.Date(t.Year(), t.Month(), t.Day(), hour, min, 0, 0, time.Local)
+}
+
+func TestParseDueText(t *testing.T) {
+	testcases := []struct {
+		Text     string
+		Expected time.Time
+	}{
+		{
+			Text:     "Due February 1, 2022 11:59 PM",
+			Expected: time.Date(2022, time.February, 1, 23, 59, 0, 0, time.Local),
+		},
+		{
+			Text:     "Due tomorrow at 2:59 PM",
+			Expected: tomorrowAt(14, 59),
+		},
+		{
+			Text:     "Due today at 3:00 PM",
+			Expected: todayAt(15, 0),
+		},
+		{
+			Text:     "期限 2022年1月26日 23:59",
+			Expected: time.Date(2022, time.January, 26, 23, 59, 0, 0, time.Local),
+		},
+		{
+			Text:     "明日 00:00 が期限",
+			Expected: tomorrowAt(0, 0),
+		},
+		{
+			Text:     "今日 23:00 が期限",
+			Expected: todayAt(23, 0),
+		},
+	}
+
+	for _, c := range testcases {
+		got, err := parseDueText(c.Text)
+		if err != nil {
+			t.Error(err)
+		} else if !got.Equal(c.Expected) {
+			t.Errorf("\n  Input string: %q\n  got : %s\n  want: %s", c.Text, got, c.Expected)
+		}
+	}
+}
