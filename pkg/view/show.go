5440ebf091484eb0c8756cd42040f46b935ed21c
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-01-28 22:43:56 +0900

    fix: きれいに整形表示

--- pkg/view/show.go
+++ pkg/view/show.go
@@ -2,59 +2,128 @@ package view
 
 import (
 	"fmt"
-
-	"sort"
+	"io"
+	"strings"
 	"time"
 
 	"github.com/arumakan1727/taskrader/pkg/assignment"
-	color "github.com/fatih/color"
+	"github.com/fatih/color"
+	"golang.org/x/text/width"
 )
 
-func Show(tasks []*assignment.Assignment) {
-	const formatHour = "2006/01/02"
-	const formatMin = "15:04"
-	wdays := []string{"日", "月", "火", "水", "木", "金", "土"}
+const (
+	borderTopL = "╭"
+	borderTopR = "╮"
+	borderBotL = "╰"
+	borderBotR = "╯"
+	borderVert = "│"
+	borderHori = "─"
+	borderVH   = "├"
+	borderHV   = "┤"
+)
 
-	sort.Slice(tasks, func(i, j int) bool {
-		return tasks[i].Deadline.Before(tasks[j].Deadline)
-	})
-
-	line := makeLine(tasks)
-
-	fmt.Println(len(tasks), "件の未提出課題：")
-	for i := 0; i < len(tasks); i++ {
-
-		limit := "<締切不明>"
-
-		if tasks[i].Deadline != time.Unix(1<<60, 999999999) {
-			limit = tasks[i].Deadline.Format(formatHour) + " (" + wdays[tasks[i].Deadline.Weekday()] + ") " + tasks[i].Deadline.Format(formatMin)
-		}
-
-		fmt.Println(line)
-		output := fmt.Sprintf("| [%d] %s |\n|     %s @%s |", i+1, tasks[i].Title, limit, tasks[i].Origin)
-		printWithColor(output, tasks[i].Deadline)
-	}
-
-	fmt.Println(line)
-
+type lineInfo struct {
+	Text  string // 行の文字列
+	Width int    // 表示幅 (e.g. "abc" なら半角3文字なので3, "あ"なら2)
 }
 
-func makeLine(tasks []*assignment.Assignment) string {
-	max := 0
+type cardContent struct {
+	lines []lineInfo
+}
 
-	for i := 0; i < len(tasks); i++ {
-		if max < len(tasks[i].Title) {
-			max = len(tasks[i].Title)
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Show(ass []*assignment.Assignment, writer io.Writer) {
+	if len(ass) == 0 {
+		color.Green("未提出の課題はありません")
+		return
+	}
+
+	cards := make([]cardContent, len(ass))
+
+	maxWidth := 0
+	for i, a := range ass {
+		l1 := fmt.Sprintf("[%02d] %s 【%s】", i+1, a.Title, a.Course)
+		l2 := fmt.Sprintf("     %s     @%s", FmtDeadline(&a.Deadline), a.Origin)
+		w1 := CalcTextWidth(l1, 1)
+		w2 := CalcTextWidth(l2, 1)
+		cards[i] = cardContent{
+			lines: []lineInfo{{l1, w1}, {l2, w2}},
+		}
+		maxWidth = max(maxWidth, w1)
+		maxWidth = max(maxWidth, w2)
+	}
+
+	// 最低でも40以上確保 || 末尾の余白は5欲しい
+	maxWidth = max(40, maxWidth+5)
+
+	writeBorderTop(writer, maxWidth+2)
+
+	bar := " " + borderVH + strings.Repeat(borderHori, maxWidth+2) + borderHV + "\n"
+
+	for i, card := range cards {
+		for _, l := range card.lines {
+			io.WriteString(writer, " "+borderVert+" ")
+			io.WriteString(writer, l.Text)
+			io.WriteString(writer, strings.Repeat(" ", maxWidth-l.Width+1))
+			fmt.Fprint(writer, borderVert+"\n")
+		}
+
+		if i < len(cards)-1 {
+			io.WriteString(writer, bar)
 		}
 	}
+	writeBorderBottom(writer, maxWidth+2)
+}
 
-	line := ""
+func writeBorderTop(w io.Writer, width int) {
+	io.WriteString(w, " "+borderTopL)
+	io.WriteString(w, strings.Repeat(borderHori, width))
+	io.WriteString(w, borderTopR+"\n")
+}
 
-	for i := 0; i < max+4; i++ {
-		line += "-"
+func writeBorderBottom(w io.Writer, width int) {
+	io.WriteString(w, " "+borderBotL)
+	io.WriteString(w, strings.Repeat(borderHori, width))
+	io.WriteString(w, borderBotR+"\n")
+}
+
+// 全角なら2, 半角なら1として各文字の表示幅を求め、その総和を返す。
+// '①' や '▼' のような曖昧幅の文字は引数 `ambigousWidth` の値とする。
+func CalcTextWidth(text string, ambigousWidth int) int {
+	wsum := 0
+
+	for _, chr := range text {
+		kind := width.LookupRune(chr).Kind()
+
+		switch kind {
+		case width.Neutral, width.EastAsianNarrow, width.EastAsianHalfwidth:
+			wsum += 1
+		case width.EastAsianWide, width.EastAsianFullwidth:
+			wsum += 2
+		case width.EastAsianAmbiguous:
+			wsum += ambigousWidth
+		default:
+			panic("Unknown Kind of rune width: " + kind.String())
+		}
 	}
+	return wsum
+}
 
-	return line
+func FmtDeadline(t *time.Time) string {
+	if t.Equal(assignment.UnknownDeadline()) {
+		return "<締切不明>"
+	}
+	const layoutDay = "2006/01/02"
+	const layoutTime = "15:04"
+	wdays := []string{"日", "月", "火", "水", "木", "金", "土"}
+	return t.Format(layoutDay) + " (" + wdays[t.Weekday()] + ") " + t.Format(layoutTime)
 }
 
 func printWithColor(output string, deadline time.Time) {
--- pkg/view/show_test.go
+++ pkg/view/show_test.go
@@ -1,24 +1,44 @@
-package view
+package view_test
 
 import (
+	"os"
 	"testing"
+	"time"
 
 	"github.com/arumakan1727/taskrader/pkg/assignment"
-	"github.com/arumakan1727/taskrader/pkg/cred"
-	"github.com/joho/godotenv"
+	"github.com/arumakan1727/taskrader/pkg/view"
 )
 
-func TestShow(t *testing.T) {
-	_ = godotenv.Load("../../.env")
-	cred := cred.LoadFromEnv()
-	cred.AbortIfEmptyFieldExists()
-
-	ass, errs := assignment.FetchAll(cred)
-
-	t.Logf("エラー: %d件\n", len(errs))
-	for _, e := range errs {
-		t.Logf("%s の課題取得でエラー: %s\n", e.Origin, e.Err)
+func dummyAssignments() []*assignment.Assignment {
+	return []*assignment.Assignment{
+		{
+			Origin:   assignment.OrigGakujo,
+			Title:    "第11回 (12/22) お題",
+			Course:   "コンパイラ",
+			Deadline: time.Date(2022, time.January, 11, 24, 00, 0, 0, time.Local),
+		},
+		{
+			Origin:   assignment.OrigEdStem,
+			Title:    "当日課題(小レポート7)",
+			Course:   "データベースシステム論",
+			Deadline: assignment.UnknownDeadline(),
+		},
+		{
+			Origin:   assignment.OrigTeams,
+			Title:    "文字幅てすと① ↑△ ",
+			Course:   "科目1",
+			Deadline: time.Date(2022, time.February, 3, 23, 59, 0, 0, time.Local),
+		},
+		{
+			Origin:   assignment.OrigTeams,
+			Title:    "Ⅱ  (´・ω・｀)⚡ÄÖж",
+			Course:   "科目998233353",
+			Deadline: time.Date(2024, time.January, 30, 9, 59, 59, 0, time.Local),
+		},
 	}
+}
 
-	Show(ass)
+func TestShow(t *testing.T) {
+	ass := dummyAssignments()
+	view.Show(ass, os.Stdout)
 }
